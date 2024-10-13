aa8a1571b9df29997eed58048dc10ac6897f6fd8
TomVietCode <mcuong04.work@gmail.com>
2024-10-13 16:28:47 +0700

    Bật tắt xoay avatar

--- public/js/script.js
+++ public/js/script.js
@@ -2,15 +2,25 @@
 const aplayer = document.querySelector("#aplayer")
 if(aplayer) {
   const songInfo = JSON.parse(aplayer.getAttribute("song-info"))
-  console.log(aplayer.getAttribute("song-info"))
+  const singer = aplayer.getAttribute("singer")
+  const innerAvatar = document.querySelector(".inner-avatar")
+
   const ap = new APlayer({
     container: document.getElementById('aplayer'),
+    autoplay: true,
     audio: [{
         name: songInfo.title,
-        artist: songInfo.singer,
+        artist: singer,
         url: songInfo.audio,
         cover: songInfo.avatar,
     }]
   });
+
+  ap.on("play", () => {
+    innerAvatar.style.animationPlayState = "running"
+  })
+  ap.on("pause", () => {
+    innerAvatar.style.animationPlayState = "paused"
+  })
 }
 // End APlayer
